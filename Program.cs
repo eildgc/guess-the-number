d7bc498bcbf6596b8f65604f8109ac8adba1e706
Ely Gandara <ely.gandara.c@gmail.com>
2020-02-24 21:18:28 -0700

    Added summary for GenerateDelimitedRandomNumber and UserInput.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ namespace guess_the_number
     {
         static int number = 0; 
         static int randomNumber = 0;
+        /// <summary>
+        /// In this method we generate a random number and multiply it by the user's input number plus one
+        /// so the random number generated is between the range we need for this program.
+        /// </summary>
         static void GenerateDelimitedRandomNumber(){
             Random r = new Random();
             randomNumber = (int)Math.Floor(r.NextDouble() * (number+1));
             }
+            /// <summary>
+            /// In this method we ask to the user to enter a positive number.
+            /// </summary>
         static void UserInput(){
             Console.WriteLine("Por favor escribe un número positivo");
             Console.WriteLine("Y presiona enter");
